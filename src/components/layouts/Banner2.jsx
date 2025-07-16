2faf29303d828996ed4ce0691d0bac6da1ee41bc
Hubert Dhrubo Gomes <gomeshubert8@gmail.com>
2025-07-17 00:21:33 +0600

    Refactor Banner2 component for improved layout and responsiveness

--- src/components/layouts/Banner2.jsx
+++ src/components/layouts/Banner2.jsx
@@ -5,28 +5,28 @@ import Counter from "./Counter";
 
 const Banner2 = () => {
   return (
-    <>
-      <div className="bg-[#E8E8EA] w-[382px] lg:w-full m-auto">
-        <Container className={"py-[30px]"}>
-        <div className="lg:bg-[url(/src/assets/banner2.png)] bg-no-repeat bg-center bg-cover py-[100px]">
-          <div className="ml-[20px] relative">
-            <Flex className={""}>
-            <span className="h-[2px] w-[40px] bg-[#C32929] shadow-lg mr-[10px]"></span>
-            <p className="text-[#C32929] font-Menu font-medium">DEAL OF THE WEEK</p>
-          </Flex>
-          <p className="text-[70px] font-Menu">
-            <span className="font-bold">Spring</span> Collection
-          </p>
-          <p className="font-Menu text-[14px] font-medium relative group cursor-pointer">
-            SHOP NOW
-            <span className="absolute top-5 left-0 h-[2px] w-[10px] bg-black group-hover:w-[90px] transition-all duration-500"></span>
-          </p>
-          <Counter className={"absolute bottom-[-74px]"}/>
+    <div className="bg-[#E8E8EA] w-full">
+      <Container className="py-8 md:py-12">
+        <div className="bg-[url(/src/assets/banner2.png)] bg-no-repeat bg-center bg-cover py-12 md:py-24 lg:py-32">
+          <div className="mx-4 md:mx-8 relative">
+            <Flex className="items-center mb-4">
+              <span className="h-[2px] w-10 bg-[#C32929] shadow-lg mr-2"></span>
+              <p className="text-[#C32929] font-Menu font-medium text-sm md:text-base">
+                DEAL OF THE WEEK
+              </p>
+            </Flex>
+            <p className="text-3xl md:text-5xl lg:text-7xl font-Menu mb-2 leading-tight">
+              <span className="font-bold">Spring</span> Collection
+            </p>
+            <p className="font-Menu text-xs md:text-sm font-medium relative group cursor-pointer mb-6">
+              SHOP NOW
+              <span className="absolute top-5 left-0 h-[2px] w-2 group-hover:w-24 bg-black transition-all duration-500"></span>
+            </p>
+            <Counter className="absolute -bottom-16 left-0" />
           </div>
         </div>
       </Container>
-      </div>
-    </>
+    </div>
   );
 };
 
